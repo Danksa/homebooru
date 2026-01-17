b6321328cc90716c5d11a644f7fae24d18669be2
danksa <gdanksa@gmail.com>
2026-01-17 22:19:12 +0100

    Created basic WebSocket test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { rootRouter } from "./routes/index.js";
 import { initialize } from "./initialize.js";
+import { WebSocketServer } from "ws";
+import { createServer } from "http";
 
 const port = process.env.PORT != null ? Number(process.env.PORT) : 3000;
 
@@ -8,6 +10,38 @@ await initialize();
 
 const app = express();
 app.use("/", rootRouter);
-app.listen(port, () => {
+
+const server = createServer(app);
+
+const wss = new WebSocketServer({ noServer: true });
+wss.on("connection", (ws, req) => {
+    console.log("Connection!");
+
+    ws.send("Test");
+
+    ws.on("close", () => {
+        console.log("Websocket closed");
+    });
+});
+
+server.on("upgrade", (req, socket, head) => {
+    const path = req.url;
+    console.log("Upgrade", path);
+
+    if(path == null) {
+        socket.destroy();
+        return;
+    }
+
+    if(path === "/posts/upload-status") {
+        wss.handleUpgrade(req, socket, head, (ws) => {
+            wss.emit("connection", ws);
+        });
+    } else {
+        socket.destroy();
+    }
+});
+
+server.listen(port, () => {
     console.log(`homebooru is running on port ${port}`);
 });
